4eb02f573d7f3aa753925069d8b89c1d75de142f
Godfrey Anthony T. Serojales <gaserojales@gmail.com>
2024-01-23 12:12:29 +0800

    NFT Preview Component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,18 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          
+      },
+      boxShadow: {
+        'nft': "0px 15px 30px 10px hsla(244, 76%, 8%, 0.464)",
+      },
+      colors: {
+        "midnight-navy" : "hsl(217, 54%, 11%)",
+        "dark-turquoise" : "hsl(216, 50%, 16%)",
+        "pale-blue":"hsl(215, 51%, 70%)",
+        "bright-cyan":"hsl(178, 100%, 50%)",
+        "dark-blue":"hsl(215, 32%, 27%)",
+        "nft-white": "hsl(0, 0%, 100%)",
       },
     },
   },
